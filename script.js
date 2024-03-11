def2044e75050fdbfd754f8c96d80d02f960543a
OGMui <80379201+OGMui@users.noreply.github.com>
2024-03-11 18:16:16 +0700

    Add for loop

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const gridSizeBtn = document.getElementById("grid-size-button");
 const gridContainer = document.getElementById("grid-container");
-
+const gridCell = `<div class="cell"></div>`
 gridSizeBtn.addEventListener("click", createGrid);
 
+
+
 function createGrid() {
-    const gridSize = prompt("Please enter the grid size: ");
+    const gridSize = Number(prompt("Please enter the grid size: "));
     console.log(
         "Create grid function was called"
     )
+    console.log(typeof gridSize, gridSize)
+    for (let i = 0; i < gridSize; i++){
+        gridContainer.innerHTML += gridCell + "test" + Number(i+1);
+        console.log(i);
+    }
+    
 }
