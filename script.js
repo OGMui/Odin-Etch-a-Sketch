5b2cc40631ca2c02e898abe09a5c2d11591c8b31
OGMui <80379201+OGMui@users.noreply.github.com>
2024-03-12 13:07:27 +0700

    Add inner for loop

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const gridSizeBtn = document.getElementById("grid-size-button");
 const gridContainer = document.getElementById("grid-container");
-const gridCell = `<div class="cell"></div>`
-gridSizeBtn.addEventListener("click", createGrid);
 
 
 
-function createGrid() {
-    const gridSize = Number(prompt("Please enter the grid size: "));
+createGrid(16);
+
+gridSizeBtn.addEventListener("click", getGridSize);
+
+function getGridSize () {
+    console.log("get grid size function was called. ")
+    gridSize = Number(prompt("Please enter the grid size: "));
+    return createGrid(gridSize);
+}
+
+function createGrid(gridSize) {
     console.log(
         "Create grid function was called"
     )
+    gridContainer.innerHTML = ""; 
+    const totalCells = gridSize * gridSize;
+
+    const gridCellSize = Math.floor( 450/ gridSize);
+    console.log("grid size", gridSize);
+    console.log("totalCells", totalCells);
+    console.log("grid cell size", gridCellSize);
+    const gridCell = `<div class="cell" height="${gridCellSize}" width="${gridCellSize}">g</div>`;
+    console.log(gridCell)
+
+    
+    
+   
     console.log(typeof gridSize, gridSize)
-    for (let i = 0; i < gridSize; i++){
-        gridContainer.innerHTML += gridCell + "test" + Number(i+1);
-        console.log(i);
-    }
+    for (let i = 0; i < totalCells; i++){
+         gridContainer.innerHTML += gridCell;
+         console.log(i);
+     }
     
 }
