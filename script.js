1e04edb5937d6208d20dfb69cf2c658c76489a35
OGMui <80379201+OGMui@users.noreply.github.com>
2024-03-12 16:02:21 +0700

    Add darken  effect on cell

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 const gridSizeBtn = document.getElementById("grid-size-button");
 const gridContainer = document.getElementById("grid-container");
-const cell = document.getElementById("cell");
+const cells = document.getElementsByClassName("cell");
 
 
 createGrid(16);
 
 gridSizeBtn.addEventListener("click", getGridSize);
-cell.addEventListener("mouseenter", () => {
-    console.log(MouseEvent);
-});
+
+function ResetColor () {
+    
+}
+
+function colorCells () {
+
+const cellsArray = Array.from(cells);
+
+cellsArray.forEach(cell => {
+    
+    cell.addEventListener("mouseenter", changeCell  = (event) => {
+        console.log("change cell function was called", event);
+        let cellColor = `rgba(0, 0, 0, ${cellOpacity/10})`; 
+
+        event.target.style.backgroundColor = cellColor;
+        let cellOpacity = event.target.style.backgroundColor.slice();
+        console.log(cellOpacity)
+        if (cellOpacity < 10){
+            cellOpacity += 1;}
+
+        
+        console.log(event.target.style.backgroundColor);
+    }   
+)})
+}
 
 function getGridSize () {
+    // cells.style.backgroundColor = "white";
     console.log("get grid size function was called. ")
-    gridSize = Number(prompt("Please enter the grid size: "));
+    let gridSize = Number(prompt("Please enter the grid size: "));
+    if (gridSize > 100) {
+        gridSize = Number(prompt("Please enter a number between 1 and 100"))
+    }
+    
     return createGrid(gridSize);
 }
 
@@ -23,28 +51,30 @@ function createGrid(gridSize) {
     gridContainer.innerHTML = ""; 
     const totalCells = gridSize * gridSize;
 
-    const gridCellSize = Math.floor( 450/ gridSize);
+    const gridCellSize =( 450/ gridSize);
     const gridCellPadding = gridCellSize / 2;
-    console.log("grid size", gridSize);
-    console.log("totalCells", totalCells);
-    console.log("grid cell size", gridCellSize);
-    console.log("grid cell padding", gridCellPadding)
+    // console.log("grid size", gridSize);
+    // console.log("totalCells", totalCells);
+    // console.log("grid cell size", gridCellSize);
+    // console.log("grid cell padding", gridCellPadding)
     const gridCell = `<div class="cell" style="height: ${gridCellSize}px; width:${gridCellSize}px; padding: ${gridCellPadding}px;"></div>`;
-    console.log(gridCell)
+    // console.log(gridCell)
 
     
     
    
     console.log(typeof gridSize, gridSize)
-    
+    let gridHTML = ``;
     for (let i = 0; i < gridSize; i++){
         for (let j = 0; j < gridSize; j++) {
-            gridContainer.innerHTML += gridCell;
-            console.log("j", j);
+            gridHTML += gridCell;
+            // console.log("j", j);
         }
          
-         console.log("i", i);
+        //  console.log("i", i);
      }
     
+     gridContainer.innerHTML = gridHTML;
+     colorCells()
 }
 
