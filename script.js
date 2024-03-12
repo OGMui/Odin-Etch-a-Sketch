9fb8faa9083c168164530144fb1806f11361426a
OGMui <80379201+OGMui@users.noreply.github.com>
2024-03-12 14:22:25 +0700

    Change for loop, Add color effect on cell

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const gridSizeBtn = document.getElementById("grid-size-button");
 const gridContainer = document.getElementById("grid-container");
-
+const cell = document.getElementById("cell");
 
 
 createGrid(16);
 
 gridSizeBtn.addEventListener("click", getGridSize);
+cell.addEventListener("mouseenter", () => {
+    console.log(MouseEvent);
+});
 
 function getGridSize () {
     console.log("get grid size function was called. ")
@@ -21,19 +24,27 @@ function createGrid(gridSize) {
     const totalCells = gridSize * gridSize;
 
     const gridCellSize = Math.floor( 450/ gridSize);
+    const gridCellPadding = gridCellSize / 2;
     console.log("grid size", gridSize);
     console.log("totalCells", totalCells);
     console.log("grid cell size", gridCellSize);
-    const gridCell = `<div class="cell" height="${gridCellSize}" width="${gridCellSize}">g</div>`;
+    console.log("grid cell padding", gridCellPadding)
+    const gridCell = `<div class="cell" style="height: ${gridCellSize}px; width:${gridCellSize}px; padding: ${gridCellPadding}px;"></div>`;
     console.log(gridCell)
 
     
     
    
     console.log(typeof gridSize, gridSize)
-    for (let i = 0; i < totalCells; i++){
-         gridContainer.innerHTML += gridCell;
-         console.log(i);
+    
+    for (let i = 0; i < gridSize; i++){
+        for (let j = 0; j < gridSize; j++) {
+            gridContainer.innerHTML += gridCell;
+            console.log("j", j);
+        }
+         
+         console.log("i", i);
      }
     
 }
+
